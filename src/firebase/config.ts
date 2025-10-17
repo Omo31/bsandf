f4f2d3eb50a5981bff70c6a3c69edf9357910427
Omowumi Badmus <oluwagbengwumi@gmail.com>
2025-10-17 04:22:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3721924314-93cfd",
+  "appId": "1:538469606377:web:c0905327a8b43f069e2178",
+  "apiKey": "AIzaSyD3EkUlnsI1A2DqQCPsRRddGfw0t72_Djo",
+  "authDomain": "studio-3721924314-93cfd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "538469606377"
+};
