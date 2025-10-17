a798d650076f7cf930815eccfb83650bb35c197f
Omowumi Badmus <oluwagbengwumi@gmail.com>
2025-10-17 10:07:38 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

---
+++ src/next.config.ts
@@ -0,0 +1,54 @@
+import type {NextConfig} from 'next';
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // Allow scripts from Paystack
+  experimental: {
+    scriptingEnabled: true,
+  },
+  async headers() {
+    return isProd ? [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co",
+          },
+        ],
+      },
+    ] : [];
+  },
+};
+
+export default nextConfig;
